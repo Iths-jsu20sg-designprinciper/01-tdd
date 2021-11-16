4082131c35f6f34f65b517d2005791536197ab92
David Andersson <david.andersson@zocom.se>
2021-11-16 15:01:32 +0100

    Comments about matchers

--- src/temperature.test.js
+++ src/temperature.test.js
@@ -27,5 +27,32 @@ describe('temperature', () => {
 		expect(actual).toBe(expected)
 	})
 	// 32 F ska vara 0 C
+
+	if( Boolean(undefined) )
+	if( Boolean(null) )
+	if( false )
+/*
+	Boolean('Hejsan') -> true
+	Boolean('         ') -> true
+	Boolean('false') -> true
+	Boolean('') -> false
+
+	Boolean(0) -> false
+	Boolean(3.14) -> true
+	Boolean(NaN) -> false
+
+	Number('123') -> 123
+	Number('five') -> NaN
+
+	let products = getProducts()
+	Är products null eller ett objekt?
+	if( products )  <- kolla om variabeln har ett värde
+
+
+	Floting point: flyttal, tal med decimaler
+	(svenska tal: decimalkomma ex. 3,14)
+	(engelska tal: decimalpunkt dvs. "point" ex. 3.14)
+	Alla JavaScript-tal är flyttal
+*/
 })
 
